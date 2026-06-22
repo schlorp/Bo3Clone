d0fcb9d55ba71a228c373052bdac14b9d8ceeb40
schlorp <70897090+schlorp@users.noreply.github.com>
2026-06-22 11:02:04 +0200

    cant move mid air any more with mouse

--- Scripts/Movement/movement_node.gd
+++ Scripts/Movement/movement_node.gd
@@ -6,6 +6,8 @@ class_name MovementNode
 @export var movement_speed: float = 5.0
 @export var jump_force: float = 2.5
 
+var jump_basis: Basis
+
 var player_movement_state: Enums.PlayerMovementState = Enums.PlayerMovementState.ON_GROUND
 
 var movement_vector: Vector3 = Vector3.ZERO
@@ -16,6 +18,7 @@ signal movement_state_changed(state: Enums.PlayerMovementState)
 func _ready() -> void:
 	input_parser.connect("on_movement_input", Callable(self, "handle_movement_input"))
 	input_parser.connect("on_jump_input_just_pressed", Callable(self, "jump"))
+
 
 func handle_movement_input(vector: Vector3) -> void:
 	if player_movement_state == Enums.PlayerMovementState.IN_AIR:
@@ -28,9 +31,18 @@ func handle_movement_input(vector: Vector3) -> void:
 	movement_vector.x = direction.x
 	movement_vector.z = direction.z
 
+
 func apply_movementvector(delta: float) -> void:
-	parent_character.velocity = parent_character.transform.basis * movement_vector * movement_speed * delta 
+	if player_movement_state == Enums.PlayerMovementState.IN_AIR:
+		parent_character.velocity = jump_basis * movement_vector * movement_speed * delta 
+	else: 
+		parent_character.velocity = parent_character.transform.basis * movement_vector * movement_speed * delta 
+	print("Movement Vector: ", movement_vector)
+	print ("Transform Basis: ", parent_character.transform.basis)
+	print("Velocity: ", parent_character.velocity)
+
 	parent_character.move_and_slide()
+
 
 func apply_gravity(delta: float) -> void:
 	if !parent_character.is_on_floor():
@@ -38,15 +50,19 @@ func apply_gravity(delta: float) -> void:
 	else:
 		movement_vector.y = 0
 
+
 func _physics_process(delta: float) -> void:
 	apply_movementvector(delta)
 	apply_gravity(delta)
 	update_movement_state()
-	parent_character.move_and_slide()
+
 
 func jump() -> void:
 	if player_movement_state == Enums.PlayerMovementState.ON_GROUND:
+		jump_basis = parent_character.transform.basis
 		movement_vector.y = jump_force
+
+
 
 func update_movement_state() -> void:
 	if parent_character.is_on_floor():
