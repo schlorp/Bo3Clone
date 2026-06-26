c5bea0a766ca05fda293c8cb4c8e3e263c3aacfe
schlorp <70897090+schlorp@users.noreply.github.com>
2026-06-26 18:38:48 +0200

    added obstacles for crouching and added shape cast to check if you can uncrouch

--- Scripts/Movement/crouch_state.gd
+++ Scripts/Movement/crouch_state.gd
@@ -5,11 +5,15 @@ var animation_player: AnimationPlayer = null
 var collision_shape: CollisionShape3D = null
 var capsule_shape: CapsuleShape3D = null
 
+var shape_cast: ShapeCast3D = null
+
 var crouch_height: float = 1.0
 var crouch_radius: float = 0.25
 
 var stand_height: float = 2.0
 var stand_radius: float = 0.5
+
+var can_un_crouch: bool = true
 
 func _init(_statemachine: StateMachine) -> void:
 	super._init(_statemachine)
@@ -19,27 +23,29 @@ func _init(_statemachine: StateMachine) -> void:
 	collision_shape = state_machine.collision_shape
 	capsule_shape = collision_shape.shape as CapsuleShape3D
 
+	shape_cast = state_machine.crouch_shape_cast
+
 
 func setup_transitions() -> void:
 	add_transition(
 		Transition.new(
 			self,
 			state_machine.available_states["SprintingState"],
-			func():	return state_machine.movement_node.sprint_input && state_machine.movement_node.is_moving()
+			func():	return state_machine.movement_node.sprint_input && state_machine.movement_node.is_moving() && can_un_crouch
 		)
 	)
 	add_transition(
 		Transition.new(
 			self,
 			state_machine.available_states["WalkingState"],
-			func():	return state_machine.movement_node.is_moving() && !state_machine.movement_node.crouch_input || Input.is_action_just_pressed("game_jump")
+			func():	return state_machine.movement_node.is_moving() && !state_machine.movement_node.crouch_input && can_un_crouch || Input.is_action_just_pressed("game_jump") && can_un_crouch
 		)
 	)
 	add_transition(
 		Transition.new(
 			self,
 			state_machine.available_states["IdleState"],
-			func():	return !state_machine.movement_node.is_moving() && !state_machine.movement_node.crouch_input
+			func():	return !state_machine.movement_node.is_moving() && !state_machine.movement_node.crouch_input && can_un_crouch
 		)
 	)
 
@@ -52,6 +58,9 @@ func enter_state() -> void:
 	capsule_shape.height = crouch_height
 	capsule_shape.radius = crouch_radius
 	collision_shape.position.y = (crouch_height - stand_height) / 2.0
+
+func update_state(delta: float) -> void:
+	can_un_crouch = !state_machine.movement_node.crouch_input && !shape_cast.is_colliding()
 
 func exit_state() -> void:
 	animation_player.play("Crouch", -1.0 ,-1.0, true)
