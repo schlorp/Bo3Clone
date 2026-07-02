73d8c5c555f859863184c0a464189abdaabc736f
schlorp <70897090+schlorp@users.noreply.github.com>
2026-07-02 13:56:10 +0200

    basic resource setup

--- Scripts/GunSystem/BaseGun/base_gun.gd
+++ Scripts/GunSystem/BaseGun/base_gun.gd
@@ -2,3 +2,25 @@ extends Node
 class_name BaseGun
 
 var gun_data: WeaponResource
+
+var magazine_ammo: int
+var reserve_ammo: int
+var current_ammo: int
+
+var fire_rate: float
+var damage: int
+
+var reload_time: float
+var reload_time_empty: float
+
+
+func _ready() -> void:
+	magazine_ammo = gun_data.magazine_ammo
+	reserve_ammo = gun_data.reserve_ammo
+	current_ammo = magazine_ammo
+
+	fire_rate = gun_data.fire_rate
+	damage = gun_data.damage
+
+	reload_time = gun_data.reload_time
+	reload_time_empty = gun_data.reload_time_empty
--- Scripts/GunSystem/weapon_resource.gd
+++ Scripts/GunSystem/weapon_resource.gd
@@ -1,2 +1,14 @@
 extends Resource
 class_name WeaponResource
+
+@export_subgroup("Ammo stats")
+@export var magazine_ammo: int = 30
+@export var reserve_ammo: int = 90
+
+@export_subgroup("Gun stats")
+@export var fire_rate: float = 6.0
+@export var damage: int = 10
+
+@export_subgroup("Reload stats")
+@export var reload_time: float = 2.0
+@export var reload_time_empty: float = 3.0
